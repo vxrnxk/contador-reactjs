6b303ac362e9aea2e3719ca211a15dc27ef1cdd8
Francisco V <vxrnxk@gmail.com>
2020-06-07 17:14:41 -0300

    Contador

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Contador from './components/Contador';
+
+class App extends Component {
+    render() {
+        return <Contador />
+    }
+}
+
+export default App;
